ddfe1f6d90a9d77bbf0c4f90b41b21019a32c33b
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-05-27 10:49:08 +0530

    changees

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,48 +8,48 @@ import { SessionData, defaultSession, sessionOptions } from "./lib";
 // ADD THE GETSESSION ACTION
 export async function getSession() {
     const session = await getIronSession<SessionData>(cookies(), sessionOptions);
-  
+
     // If user visits for the first time session returns an empty object.
     // Let's add the isLoggedIn property to this object and its value will be the default value which is false
     if (!session.isLoggedIn) {
-      session.isLoggedIn = defaultSession.isLoggedIn;
+        session.isLoggedIn = defaultSession.isLoggedIn;
     }
-  
-    return session;
-  }
-  
 
-  export async function login(
+    return session;
+}
+
+
+export async function login(
     // THIS IS THE PARAMETER THAT WE NEED TO ADD
     prevState: { error: undefined | string },
     formData: FormData
-  ) {
+) {
     const session = await getSession();
-  
+
     const formUsername = formData.get("username") as string;
     const formPassword = formData.get("password") as string;
-  
+
     const user = {
-      id:1,
-      username:formUsername,
-      img:"avatar.png"
+        id: 1,
+        username: formUsername,
+        img: "avatar.png"
     }
-  
-    if(!user){
-      // IF THERE IS AN ERROR THE STATE WILL BE UPDATED
-      return { error: "Wrong Credentials!" }
+
+    if (!user.username) {
+        // IF THERE IS AN ERROR THE STATE WILL BE UPDATED
+        return { error: "Wrong Credentials!" }
     }
-  
+
     session.isLoggedIn = true;
     session.userId = user.id;
     session.username = user.username;
-  
+
     await session.save();
     redirect("/")
-  }
+}
 
-  export async function logout() {
+export async function logout() {
     const session = await getSession();
     session.destroy();
-    redirect("/")
-  }
+    redirect("/login")
+}
