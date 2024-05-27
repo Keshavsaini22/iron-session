a0c440fcc2b292cd89b15d47457e0661a60c5a39
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-05-27 10:41:01 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import { redirect } from "next/navigation";
+import { getSession } from "../actions";
+import LoginForm from "@/components/loginform/page";
+
+const LoginPage = async () => {
+  const session = await getSession();
+
+  if (session.isLoggedIn) {
+    redirect("/");
+  }
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage
